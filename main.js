ffd9ff327075961d70a6057b17d4bae363b8bbb0
TheEggShark <theeggshark@gmail.com>
2021-09-12 10:54:54 -0500

    unstashed bc i dont know git

--- main.js
+++ main.js
@@ -1,40 +1,19 @@
 Game.registerMod("Auto CCS",{
     init:function(){
-
         Game.Notify(`CCS loaded!`,'',[16,5]);
-        this.Objects = ["Cursor","Grandma","Farm","Mine","Factory","Bank","Temple","Wizard tower","Shipment","Alchemy lab","Portal","Time machine","Antimatter condenser","Prism","Chancemaker","Fractal engine","Javascript console","Idleverse"]
         let MOD = this
-        MOD.CPS = {
-            "Cursor":0,
-            "Grandma":0,
-            "Farm":0,
-            "Mine":0,
-            "Factory":0,
-            "Bank":0,
-            "Temple":0,
-            "Wizard tower":0,
-            "Shipment":0,
-            "Alchemy lab":0,
-            "Portal":0,
-            "Time machine":0,
-            "Antimatter condenser":0,
-            "Prism":0,
-            "Chancemaker":0,
-            "Fractal engine":0,
-            "Javascript console":0,
-            "Idleverse":0
-        }
-        MOD.Updatecps();
-        console.log(MOD.CPS["Grandma"])
         MOD.starter = false
-        MOD.Bestbuilding = "Cursor"
+        MOD.Bestbuilding = "Grandma"
+        // BAsicly we have to hard code it even CM does it so my idea is wacky                                         18                                 25                       30                                                40                                                50                                                60                                                70                       75                       80                                                90                                               100                                                110                                               120                                               130                                               140                                               150                                               160                                               160                                               170                                               180                                               190                                                    200                                               210                                               220                                               230                                               240                                          250                                               260                                               270                                               280                                               290                                               300 NEARLY HALF WAY!                              310                                               320                                               330                                               340                                               350                                               360             jank                              370                                               380                                               390                                               400
+        MOD.upgradetypes = ["x2","x2","x2","tf","tf","tf","tf","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","km","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","tf","x2","x2","x2","x2","x2","x2","x2","x2","ig","ig","km","fc","fc","gt","gt","gt","gt","gt","gt","gt","rt","tr","rt","rt","rt","rt","rt","rt","rt","rt","ig","cb","cb","cb","cb","ig","fc","fc","tf","ig","ig","ig","ig","rt","fc","fc","fc","ig","fc","fc","fc","fc","fc","fc","fc","x2","x2","x2","x2","gt","fc","fc","fc","fc","km","tf","x2","x2","x2","x2","x2","x2","x2","x2","x2","cb","fc","fc","fc","fc","fc","fc","fc","fc","fc","hc","hc","hc","hc","hc","fc","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","fc","fc","fc","fc","fc","fc","x2","x2","x2","x2","x2","gt","ig","ig","ig","ig","ig","ig","km","tf","tf","cb","cb","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","cb","ig","ig","ig","ig","fc","fc","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","gt","gt","gt","tn","tn","tn","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","km","km","ig","cd","ig","ig","ig","ig","ig","fc","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","cb","cb","ig","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","ig","ig","ig","ig","ig","ig","ig","fc"]
+        // x2 = a times to upgrade | tf = thousnad fingers upgrade | km is a milk upgrade | fc = flavored cookie | ig are ignored upgrades we only use this to save a spot an expample would be GC upgrades as quaintifiblity is hard of these could be added later ig | gt = grandama type | rt = reasherch ones | cb = clicking boost | hc = hevlany chip boost the stuff that make asenstion better | st =stanta upgrades | cd = cookie drangon upgrades | su = synergy upgrades
         l('storeTitle').insertAdjacentHTML('beforeend','<a style="font-size:12px;position:absolute;bottom:2px;right:2px;display:block;" class="smallFancyButton" id="storeClicker"></a>');
 
         AddEvent(l('storeClicker'),'click',function(){
             if (MOD.starter == true) {
-                MOD.starter = false
+                MOD.starter = false;
             } else {
-                MOD.starter = true
+                MOD.starter = true;
             }
 		});
         // add all init functions before here
@@ -57,11 +36,10 @@ Game.registerMod("Auto CCS",{
     },
     buildingPicker:function(){
         score = 0.0
-        this.Updatecps();
-        for (building in this.CPS) {
-            if (this.buildingMath(building) >= score) {
-                score = this.buildingMath(building);
-                this.Bestbuilding = building
+        for (Objects in Game.Objects) {
+            if (this.buildingMath(Objects) >= score) {
+                score = this.buildingMath(Objects);
+                this.Bestbuilding = Objects;
 
             }
         }
@@ -70,21 +48,18 @@ Game.registerMod("Auto CCS",{
         }
     },
     Updatecps:function(){
-        for (building in this.CPS) {
-            this.CPS[building] = Game.Objects[`${building}`].storedCps
+        for (Objects in Game.Objects) {
+            this.CPS[Objects] = Game.Objects[`${Objects}`].storedCps;
         }
     },
     getPrice:function(building){
-        return Game.Objects[`${building}`].price
+        return Game.Objects[`${building}`].price;
     },
     buildingMath:function(building){
-        return this.CPS[building] / this.getPrice(building)
+        return Game.Objects[`${building}`].storedCps / this.getPrice(building);
     },
     test:function(){
-        console.log(this.CPS)
-        for(building in this.CPS){
-            console.log(building)
-        }
+
     },
     save:function(){
         //use this to store persistent data associated with your mod
