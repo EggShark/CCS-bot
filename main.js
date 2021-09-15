6add8b7ab138932fabccb205d4a207b56f655358
TheEggShark <ther3al3ggshark@gmail.com>
2021-09-15 17:26:02 -0500

    Upgrade functions work now and it autobuys upgrades and buildings

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ Game.registerMod("Auto CCS",{
         MOD.starter = false
         MOD.Bestbuilding = "Grandma"
         MOD.tfboost = 0.0
+        MOD.buildingsScore = 0.0
+        MOD.UpgradeScore = 0.0
         // BAsicly we have to hard code it even CM does it so my idea is wacky                                         18                                 25                       30                                                40                                                50                                                60                                                70                       75                       80                                                90                                               100                                                110                                               120                                               130                                               140                                               150                                               160                                               160                                               170                                               180                                               190                                                    200                                               210                                               220                                               230                                               240                                          250                                               260                                               270                                               280                                               290                                               300 NEARLY HALF WAY!                              310                                               320                                               330                                               340                                               350                                               360             jank                              370                                               380                                               390                                               400                                               410                                               420 hahaha                                        430                                               440                                                450                                               460                                               470                                               480                                              490                                                500                                               510                                                    520                                               530                                               540                                               550                                               560                                               570                                               580                                               590                                               600                                               610                                               620                                               630                                               640                                               650                                               660 last cb&tf btw       666                 670                                               680                                               690                                               700                                               710                                               720
         MOD.upgradetypes = ["x2","x2","x2","tf","tf","tf","tf","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","km","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","tf","x2","x2","x2","x2","x2","x2","x2","x2","ig","ig","km","fc","fc","gt","gt","gt","gt","gt","gt","gt","rt","tr","rt","rt","rt","rt","rt","rt","rt","rt","ig","cb","cb","cb","cb","ig","fc","fc","tf","ig","ig","ig","ig","rt","fc","fc","fc","ig","fc","fc","fc","fc","fc","fc","fc","x2","x2","x2","x2","gt","fc","fc","fc","fc","km","tf","x2","x2","x2","x2","x2","x2","x2","x2","x2","cb","fc","fc","fc","fc","fc","fc","fc","fc","fc","hc","hc","hc","hc","hc","fc","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","fc","fc","fc","fc","fc","fc","x2","x2","x2","x2","x2","gt","ig","ig","ig","ig","ig","ig","km","tf","tf","cb","cb","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","cb","ig","ig","ig","ig","fc","fc","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","gt","gt","gt","tn","tn","tn","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","km","km","ig","cd","ig","ig","ig","ig","ig","fc","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","cb","cb","ig","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","ig","ig","ig","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","gt","x2","x2","x2","x2","x2","x2","x2","x2","su","km","fc","cb","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","su","fc","fc","fc","fc","fc","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","ig","cb","cb","km","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","ig","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","ig","gt","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","su","su","fc","fc","ig","fc","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","gt","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","su","su","fc","fc","fc","fc","fc","fc","km","fc","fc","fc","fc","fc","fc","fc","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","ig","km","fu","ig","ig","fc","ig","ig","cd","cd","cd","cd","fc","fc","fc","fc","fc","fc","fc","fc","tf","cb","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","fc","fc","fc","fc","fc","x2","gt","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","su","su","fu","fc","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc"]
         // x2 = a times to upgrade | tf = thousnad fingers upgrade | km is a milk upgrade | fc = flavored cookie | ig are ignored upgrades we only use this to save a spot an expample would be GC upgrades as quaintifiblity is hard of these could be added later ig | gt = grandama type | rt = reasherch ones | cb = clicking boost | hc = hevlany chip boost the stuff that make asenstion better | st =stanta upgrades | cd = cookie drangon upgrades | su = synergy upgrades | fu = fortune upgrades
@@ -28,7 +30,8 @@ Game.registerMod("Auto CCS",{
     },
     mainLoop:function(){
         this.clicker();
-        this.buildingPicker();    
+        this.buildingPicker();
+        this.upgradepicker();
     },
     clicker:function(){
         Game.lastClick = Game.time-5;
@@ -41,27 +44,59 @@ Game.registerMod("Auto CCS",{
             if (this.buildingMath(Objects) >= score) {
                 score = this.buildingMath(Objects);
                 this.Bestbuilding = Objects;
+                this.buildingsScore = score
 
             }
         }
-        if (Game.cookies >= this.getPrice(this.Bestbuilding)){
-            Game.Objects[`${this.Bestbuilding}`].buy();
+        if (Game.cookies > this.getPrice(this.Bestbuilding)){
+            Game.Objects[`${this.Bestbuilding}`].buy(1);
+
         }
     },
     upgradepicker:function(){
         score = 0.0
         for (let i = 0; i < Game.UpgradesInStore.length; i++) {
             var storeObj = Game.UpgradesInStore[i];
-            if(calculateGains(this.upgradetypes[storeObj.id],storeObj.id) >= score) {
-
+            if(this.calculateGains(this.upgradetypes[storeObj.id],storeObj.id) >= score) {
+                score = this.calculateGains(this.upgradetypes[storeObj.id],storeObj.id);
+                this.BestUpgrades = storeObj.id;
+                this.UpgradeScore = score
             }
         }
+        if (this.BestUpgrades != undefined) {
+            if (Game.cookies >= this.getUpPrice(this.BestUpgrades) && this.UpgradeScore >= this.buildingsScore){
+                if (this.upgradetypes[this.BestUpgrades] == "tf") {
+                    Game.UpgradesById[this.BestUpgrades].buy();
+                    this.tfboost = this.getTfBoost(this.BestUpgrades, this.tfboost);
+                }
+                else{
+                    Game.UpgradesById[this.BestUpgrades].buy();
+                }
+            }
+        }
+    },
+    getUpPrice:function(id){
+        return Game.UpgradesById[id].basePrice;
     },
     getPrice:function(building){
         return Game.Objects[`${building}`].price;
     },
     buildingMath:function(building){
-        return Game.Objects[`${building}`].storedCps / this.getPrice(building);
+        return Game.Objects[`${building}`].storedCps/this.getPrice(building);
+    },
+    getTfBoost:function(id, currentBoost){
+        if(id == 3){
+            return 0.1;
+        }
+        else if(id == 4){
+            return currentBoost* 5;
+        }
+        else if(id == 5){
+            return currentBoost* 10;
+        }
+        else{
+            return currentBoost* 20;
+        }
     },
     x2upgrades:function(id){
         price = Game.UpgradesById[id].getPrice();
@@ -120,12 +155,12 @@ Game.registerMod("Auto CCS",{
         return Game.computedMouseCps * 15;
     },
     GrandmaTypeMath:function(id){
-        incremental = Game.UpgradesById[id].buildingTie.id - 1; //sneaky work around for not having to hard code
+        incremetnal = Game.UpgradesById[id].buildingTie.id - 1; //sneaky work around for not having to hard code
         return(Game.UpgradesById[id].buildingTie.storedTotalCps * (.01 *(Math.floor(Game.Objects.Grandma.amout/ incremetnal))));
     },
     clickBoostMath:function(id){
         price = Game.UpgradesById[id].price;
-        currentClicks = clickCps();
+        currentClicks = this.clickCps();
         boostamount = Game.buildingCps / 100;
         return((currentClicks * boostamount)/price);
     },
@@ -155,7 +190,7 @@ Game.registerMod("Auto CCS",{
     dragonUpgrades:function(id){
         price = Game.UpgradesById[id].price
         if(id == 324){
-            return 1; //just buy the dragon bc yes and its only 25 cookies why quantify it
+            return 1; // just buy the dragon bc yes and its only 25 cookies why quantify it
         }
         else if(id == 648){
             boost = Game.buildingCps * 0.03;
@@ -214,7 +249,7 @@ Game.registerMod("Auto CCS",{
     },
     calculateGains:function(listitem,id){
         switch (listitem) {
-            case "x2": //x2 upgrades
+            case "x2": // x2 upgrades
                 return this.x2upgrades(id);
             case "gt": // grandma types
                 return this.GrandmaTypeMath(id);
@@ -223,7 +258,7 @@ Game.registerMod("Auto CCS",{
             case "tf": // thousand fingers
                 return this.tfmath(id);
             case "cb": // clicking boost upgrages
-                return clickBoostMath(id);
+                return this.clickBoostMath(id);
             case "km": // kitten upgrages
                 return this.milkMath(id);
             case "su": // synergy upgrades
@@ -236,8 +271,8 @@ Game.registerMod("Auto CCS",{
                 return this.HevanlyChips(id);
             case "st": // Santa upgrades
                 return this.santaMath(id);
-            default:
-                break;
+            default: // any errors in list as well as ig upgrades will just retrun 0 and nothing will happen
+                return 0;
         }
     },
     save:function(){
