236ead6fb65bb1c162718f1150a4482e17396a97
TheEggShark <ther3al3ggshark@gmail.com>
2021-09-19 14:02:10 -0500

    info is in the info menu and GC auto clicker TF bug fixed

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
 Game.registerMod("Auto CCS",{
     init:function(){
+        
         Game.Notify(`CCS loaded!`,'',[16,5]);
-        let MOD = this
-        MOD.starter = false
-        MOD.Bestbuilding = "Grandma"
-        MOD.tfboost = 0.0
-        MOD.buildingsScore = 0.0
-        MOD.UpgradeScore = 0.0
+        let MOD = this;
+        Game.starter = false
+        Game.Switcher = function(){
+            if (Game.starter == false) {
+                Game.starter = true
+            } else {
+                Game.starter = false
+            }
+        }
+        MOD.defaultInfo = Game.updateLog;
+        MOD.Bestbuilding = "Grandma";
+        MOD.infoWrite();
+        MOD.tfboost = 0.0;
+        MOD.buildingsScore = 0.0;
+        MOD.UpgradeScore = 0.0;
         // BAsicly we have to hard code it even CM does it so my idea is wacky                                         18                                 25                       30                                                40                                                50                                                60                                                70                       75                       80                                                90                                               100                                                110                                               120                                               130                                               140                                               150                                               160                                               160                                               170                                               180                                               190                                                    200                                               210                                               220                                               230                                               240                                          250                                               260                                               270                                               280                                               290                                               300 NEARLY HALF WAY!                              310                                               320                                               330                                               340                                               350                                               360             jank                              370                                               380                                               390                                               400                                               410                                               420 hahaha                                        430                                               440                                                450                                               460                                               470                                               480                                              490                                                500                                               510                                                    520                                               530                                               540                                               550                                               560                                               570                                               580                                               590                                               600                                               610                                               620                                               630                                               640                                               650                                               660 last cb&tf btw       666                 670                                               680                                               690                                               700                                               710                                               720
         MOD.upgradetypes = ["x2","x2","x2","tf","tf","tf","tf","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","km","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","tf","x2","x2","x2","x2","x2","x2","x2","x2","ig","ig","km","fc","fc","gt","gt","gt","gt","gt","gt","gt","rt","tr","rt","rt","rt","rt","rt","rt","rt","rt","ig","cb","cb","cb","cb","ig","fc","fc","tf","ig","ig","ig","ig","rt","fc","fc","fc","ig","fc","fc","fc","fc","fc","fc","fc","x2","x2","x2","x2","gt","fc","fc","fc","fc","km","tf","x2","x2","x2","x2","x2","x2","x2","x2","x2","cb","fc","fc","fc","fc","fc","fc","fc","fc","fc","hc","hc","hc","hc","hc","fc","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","fc","fc","fc","fc","fc","fc","x2","x2","x2","x2","x2","gt","ig","ig","ig","ig","ig","ig","km","tf","tf","cb","cb","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","cb","ig","ig","ig","ig","fc","fc","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","gt","gt","gt","tn","tn","tn","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","km","km","ig","cd","ig","ig","ig","ig","ig","fc","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","cb","cb","ig","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","ig","ig","ig","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","gt","x2","x2","x2","x2","x2","x2","x2","x2","su","km","fc","cb","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","su","fc","fc","fc","fc","fc","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","ig","cb","cb","km","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","ig","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","ig","gt","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","su","su","fc","fc","ig","fc","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","gt","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","su","su","fc","fc","fc","fc","fc","fc","km","fc","fc","fc","fc","fc","fc","fc","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","ig","km","fu","ig","ig","fc","ig","ig","cd","cd","cd","cd","fc","fc","fc","fc","fc","fc","fc","fc","tf","cb","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","fc","fc","fc","fc","fc","x2","gt","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","su","su","fu","fc","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc"]
         // x2 = a times to upgrade | tf = thousnad fingers upgrade | km is a milk upgrade | fc = flavored cookie | ig are ignored upgrades we only use this to save a spot an expample would be GC upgrades as quaintifiblity is hard of these could be added later ig | gt = grandama type | rt = reasherch ones | cb = clicking boost | hc = hevlany chip boost the stuff that make asenstion better | st =stanta upgrades | cd = cookie drangon upgrades | su = synergy upgrades | fu = fortune upgrades
-        l('storeTitle').insertAdjacentHTML('beforeend','<a style="font-size:12px;position:absolute;bottom:2px;right:2px;display:block;" class="smallFancyButton" id="storeClicker"></a>');
-
-        AddEvent(l('storeClicker'),'click',function(){
-            if (MOD.starter == true) {
-                MOD.starter = false;
-            } else {
-                MOD.starter = true;
-            }
-		});
         // add all init functions before here
         Game.registerHook('logic',function(){
-            if (MOD.starter == true) {
+            if (Game.starter == true) {
                 MOD.mainLoop();
             }
 
-        }); 
-
+        }); ;
     },
     mainLoop:function(){
+        this.infoCheck();
+        this.infoWrite();
         this.clicker();
         this.buildingPicker();
         this.upgradepicker();
+        this.GCLogic();
     },
     clicker:function(){
         Game.lastClick = Game.time-5;
@@ -131,7 +134,7 @@ Game.registerMod("Auto CCS",{
         power = (Game.UpgradesById[id].power / 100) + 1;
         return((Game.buildingCps * power)/price);
     },
-    tfmath:function(){
+    tfmath:function(id){
         currentClicks = this.clickCps();
         temptfboost = this.tfboost;
         if(id == 3){
@@ -274,6 +277,42 @@ Game.registerMod("Auto CCS",{
             default: // any errors in list as well as ig upgrades will just retrun 0 and nothing will happen
                 return 0;
         }
+    },
+    // end of upgrade code on to GC code
+    GCLogic:function(){
+        Game.shimmers.forEach(function(shimmer) {
+            if (shimmer.type == "golden") {
+                shimmer.pop();
+            }
+        })
+    },
+    // wow GC hard
+    infoCheck:function(){
+        if (Game.onMenu == "log") {
+            this.startButton = l('rows').insertAdjacentHTML('afterbegin','<div> <a style="font-size:12px;position:absolute;top:2px;right:2px;display:block;visibility:visible;" class="smallFancyButton" id="storeClicker">Start automation</a></div>');
+        }
+        else {
+            this.startButton = l('rows').insertAdjacentHTML('afterbegin','<div> <a style="font-size:12px;position:absolute;top:2px;right:2px;display:block;visibility: hidden;" class="smallFancyButton" id="storeClicker">Start automation</a></div>');
+        }
+    },
+    infoWrite:function(){
+        Game.updateLog = `
+        <div class="selectable"
+            <div class="section">
+                <div class="title">AUTO-CCS info  
+                    <div class='listing'> 
+                        best building: ${this.Bestbuilding} score: ${this.buildingsScore}
+                    </div>
+                    <div class='listing'>
+                        best upgrades here
+                    </div>
+                    <div class='listing' id='buttonZone'>
+                        <a class='smallFancyButton' onclick='Game.Switcher();'> testing </a>
+                    </div>
+                </div>
+            </div>
+        </div>` + this.defaultInfo;
+        Game.UpdateMenu();
     },
     save:function(){
         //use this to store persistent data associated with your mod
