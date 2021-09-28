458c14a929e18aec3bf7f0a2519c81e8cdfe1b21
TheEggShark <ther3al3ggshark@gmail.com>
2021-09-28 17:12:56 -0500

    minor bug fixes

--- main.js
+++ main.js
@@ -19,6 +19,8 @@ Game.registerMod("Auto CCS",{
         MOD.tfboost = 0.0;
         MOD.buildingsScore = 0.0;
         MOD.UpgradeScore = 0.0;
+        //MOD.Garden = Game.Objects.Farm.minigame
+        //MOD.seeds = MOD.Garden.plantsById
         // BAsicly we have to hard code it even CM does it so my idea is wacky                                         18                                 25                       30                                                40                                                50                                                60                                                70                       75                       80                                                90                                               100                                                110                                               120                                               130                                               140                                               150                                               160                                               160                                               170                                               180                                               190                                                    200                                               210                                               220                                               230                                               240                                          250                                               260                                               270                                               280                                               290                                               300 NEARLY HALF WAY!                              310                                               320                                               330                                               340                                               350                                               360             jank                              370                                               380                                               390                                               400                                               410                                               420 hahaha                                        430                                               440                                                450                                               460                                               470                                               480                                              490                                                500                                               510                                                    520                                               530                                               540                                               550                                               560                                               570                                               580                                               590                                               600                                               610                                               620                                               630                                               640                                               650                                               660 last cb&tf btw       666                 670                                               680                                               690                                               700                                               710                                               720
         MOD.upgradetypes = ["x2","x2","x2","tf","tf","tf","tf","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","km","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","tf","x2","x2","x2","x2","x2","x2","x2","x2","ig","ig","km","fc","fc","gt","gt","gt","gt","gt","gt","gt","rt","tr","rt","rt","rt","rt","rt","rt","rt","rt","ig","cb","cb","cb","cb","ig","fc","fc","tf","ig","ig","ig","ig","rt","fc","fc","fc","ig","fc","fc","fc","fc","fc","fc","fc","x2","x2","x2","x2","gt","fc","fc","fc","fc","km","tf","x2","x2","x2","x2","x2","x2","x2","x2","x2","cb","fc","fc","fc","fc","fc","fc","fc","fc","fc","hc","hc","hc","hc","hc","fc","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","st","fc","fc","fc","fc","fc","fc","x2","x2","x2","x2","x2","gt","ig","ig","ig","ig","ig","ig","km","tf","tf","cb","cb","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","cb","ig","ig","ig","ig","fc","fc","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","gt","gt","gt","tn","tn","tn","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","km","km","ig","cd","ig","ig","ig","ig","ig","fc","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","ig","cb","cb","ig","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","su","ig","ig","ig","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","ig","ig","ig","gt","x2","x2","x2","x2","x2","x2","x2","x2","su","km","fc","cb","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","su","fc","fc","fc","fc","fc","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","ig","cb","cb","km","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","fc","fc","fc","fc","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","km","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","ig","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","ig","gt","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","su","su","fc","fc","ig","fc","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","fc","ig","ig","gt","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","su","su","fc","fc","fc","fc","fc","fc","km","fc","fc","fc","fc","fc","fc","fc","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","fu","ig","km","fu","ig","ig","fc","ig","ig","cd","cd","cd","cd","fc","fc","fc","fc","fc","fc","fc","fc","tf","cb","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","fc","fc","fc","fc","fc","x2","gt","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","su","su","fu","fc","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","x2","ig","ig","ig","ig","fc","fc","fc","fc","fc","fc","fc","fc"]
         // x2 = a times to upgrade | tf = thousnad fingers upgrade | km is a milk upgrade | fc = flavored cookie | ig are ignored upgrades we only use this to save a spot an expample would be GC upgrades as quaintifiblity is hard of these could be added later ig | gt = grandama type | rt = reasherch ones | cb = clicking boost | hc = hevlany chip boost the stuff that make asenstion better | st =stanta upgrades | cd = cookie drangon upgrades | su = synergy upgrades | fu = fortune upgrades
@@ -52,19 +54,15 @@ Game.registerMod("Auto CCS",{
             }
         }
 
-        if (Game.cookies > this.getPrice(this.Bestbuilding)){
-            if (Game.buyMode == 1) {
-                Game.Objects[`${this.Bestbuilding}`].buy();
-                this.newthing = true
-                this.infoBuildingUdater();
-            }
-            else{
+        if (Game.cookies > this.getPrice(this.Bestbuilding) && this.buildingsScore >= this.UpgradeScore){
+            currBuyMode = Game.buyMode;
+            if (Game.buyMode == -1) {
                 Game.buyMode = 1
-                Game.Objects[`${this.Bestbuilding}`].buy();
-                this.newthing = true
-                this.infoBuildingUdater();
-                Game.buyMode = -1
-            } //this is to make sure the info menu is accurate
+            }
+            Game.Objects[`${this.Bestbuilding}`].buy();
+            this.newthing = true;
+            this.infoBuildingUdater();
+            Game.buyMode = currBuyMode;
         }
     },
     upgradepicker:function(){
@@ -140,6 +138,10 @@ Game.registerMod("Auto CCS",{
     },
     x2upgrades:function(id){
         price = Game.UpgradesById[id].getPrice();
+        if (id in [0,1,2]) {
+            totalCps = Game.Objects.Cursor.storedTotalCps;
+            return(((this.clickCps() + totalCps) * 2)/price); 
+        }
         totalCps = Game.UpgradesById[id].buildingTie.storedTotalCps;
         return(totalCps * 2 / price);
     },
@@ -374,6 +376,9 @@ Game.registerMod("Auto CCS",{
             </div>
         </div>` + this.defaultInfo;
         Game.UpdateMenu();
+    },
+    gardenHelper:function(){
+
     },
     save:function(){
         //use this to store persistent data associated with your mod
